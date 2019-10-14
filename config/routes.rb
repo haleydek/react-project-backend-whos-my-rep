ca9f9657f8319fb94b835a9dc077167ef8b5d944
haleydek <hcd09@live.com>
2019-10-14 16:38:10 -0500

    create user and session routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/users/:id' to: 'users#show'
+
+  post '/signup' to: 'users#create'
+
+  post '/login' to: 'sessions#create'
+
+  delete '/logout' to: 'sessions#destroy'
 end
