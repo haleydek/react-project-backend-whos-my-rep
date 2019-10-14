23e5a3df49d2417e0dccde83bc12384b21365115
haleydek <hcd09@live.com>
2019-10-14 16:12:10 -0500

    enable CORS

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,14 @@ module ReactProjectBackendWhosMyRep
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
+    # Enables testing of API during local development
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+          origins '*'
+          resource '*', headers: :any, methods: [:get, :post, :patch, :delete]
+      end
+    end
+
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
