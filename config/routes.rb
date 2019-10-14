0d86460b61e92c2ebdc67590d2654486b9d05808
haleydek <hcd09@live.com>
2019-10-14 16:51:29 -0500

    add commas to routes to resolve syntax error

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/users/:id' to: 'users#show'
+  get '/users/:id', to: 'users#show'
 
-  post '/signup' to: 'users#create'
+  post '/signup', to: 'users#create'
 
-  post '/login' to: 'sessions#create'
+  post '/login', to: 'sessions#create'
 
-  delete '/logout' to: 'sessions#destroy'
+  delete '/logout', to: 'sessions#destroy'
 end
