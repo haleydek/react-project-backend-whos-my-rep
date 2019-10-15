8a523c3014a488234e13c27fb538455a1830fecd
haleydek <hcd09@live.com>
2019-10-15 09:31:09 -0500

    add email uniqueness validation to User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
     has_many :badges, through: :users_badges
 
     validates :email, presence: true
+    validates :email, uniqueness: true
     validates :email, format: /@/
     validates :first_name, presence: true
 end
