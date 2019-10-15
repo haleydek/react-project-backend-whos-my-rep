e90c3067a4f0fced5323026acad8a4d7db56d052
haleydek <hcd09@live.com>
2019-10-14 21:20:37 -0500

    add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
     has_many :users_badges
     has_many :badges, through: :users_badges
+
+    validates :email, presence: true
+    validates :email, format: /@/
+    validates :first_name, presence: true
 end
