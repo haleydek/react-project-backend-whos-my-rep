7ff968035e76bf28a3aeba008137b6bbb8d0d788
haleydek <hcd09@live.com>
2019-10-15 08:31:59 -0500

    add csrf protection. build current_user and log_in helper methods.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+    protect_from_forgery with: :exception
+
+    helper_method :current_user, :log_in
+
+    def current_user
+        @current_user ||= User.find_by(id: session[:user_id])
+    end
+
+    def log_in(user)
+        session[:user_id] = user.id
+    end
 end
