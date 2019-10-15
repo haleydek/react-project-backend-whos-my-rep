0f7edc7cdfdaf49ffdd85e68c2cf7095db3cdfa7
haleydek <hcd09@live.com>
2019-10-15 08:54:45 -0500

    build #create action in sessions controller.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,18 @@
 class SessionsController < ApplicationController
+    def create
+        user = User.find_by(email: params[:email])
+
+        if user
+            log_in(user)
+
+            render json: user.to_json(
+                :include => {
+                    :badges => { only: [:id] }
+                },
+                except: [:created_at, :updated_at]
+            )
+        else
+            render json: { :message => "Incorrect email"}
+        end
+    end
 end
