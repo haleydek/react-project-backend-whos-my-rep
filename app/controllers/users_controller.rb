67e1b43103b30a06a2076b398f48c9393d58f86d
haleydek <hcd09@live.com>
2019-10-18 12:56:11 -0500

    build users#create action.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,17 @@
 class UsersController < ApplicationController
+    def create
+        new_user = User.new(first_name: params[:firstName], email: params[:email])
+
+        if new_user.save!
+            render json: new_user.to_json(
+                except: [:created_at, :updated_at],
+                methods: :badge_ids
+            )
+        else
+            render json: { :message => errors.full_messages.join(". ") }
+        end
+    end
+
     def show
         user = User.find_by(id: params[:id])
 
